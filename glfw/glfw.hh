2d8871b08998940343ee0555eb5f2951fb810f86
Mark Sibly <marksibly@gmail.com>
2022-09-12 22:09:55 +1200

    Fix Linux build.

--- glfw/glfw.hh
+++ glfw/glfw.hh
@@ -6,17 +6,8 @@
 #undef max
 #endif
 
+#if OS_LINUX
+#include <opengl/opengl.hh>
+#endif
+
 #include <GLFW/glfw3.h>
-
-#ifndef OS_EMSCRIPTEN
-#if OS_WINDOWS
-#define GLFW_EXPOSE_NATIVE_WIN32 1
-#define GLFW_EXPOSE_NATIVE_WGL 1
-#elif OS_LINUX
-#define GLFW_EXPOSE_NATIVE_X11 1
-#define GLFW_EXPOSE_NATIVE_GLX 1
-#endif
-
-#include <GLFW/glfw3native.h>
-
-#endif
